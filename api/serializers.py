3baf5303d4299aa86916eb4e21a4b776a135c97b
Lppetrov0 <135375723+Lppetrov0@users.noreply.github.com>
2024-06-16 19:56:40 +0300

    Delete api directory

--- api/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from home.models import Question,Answer
-
-
-class QuestionSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Question
-        fields = '__all__'
