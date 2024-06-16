af01738828f84f90001dca383dd3f7ae1d202b59
Lppetrov0 <135375723+Lppetrov0@users.noreply.github.com>
2024-06-16 19:56:48 +0300

    Delete home directory

--- home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.home,name = 'home'),
-    path('question/<str:pk>/',views.question, name = 'questions_page'),
-]
